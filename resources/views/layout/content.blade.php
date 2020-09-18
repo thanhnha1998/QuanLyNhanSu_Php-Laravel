575a1670034e966edad2cfd1ef14548d7a6b92b2
thanhnha1998 <64349990+thanhnha1998@users.noreply.github.com>
2020-09-19 01:01:40 +0700

    upload

---
+++ resources/views/layout/content.blade.php
@@ -0,0 +1,147 @@
+@extends('layout.index')
+@section('content')
+
+        <!-- wrapper  -->
+        <!-- ============================================================== -->
+        <div class="dashboard-wrapper" style="min-height: 0px !important">
+            <div class="dashboard-ecommerce">
+                <div class="container-fluid dashboard-content p-0 ">
+                    <!-- ============================================================== -->
+                    <!-- pageheader  -->
+                    <!-- ============================================================== -->
+                    <div class="row">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                            <div class="page-header">
+                            <div style="background-image:url(upload/slide/achievement-agreement-arms-1068523.jpg); background-repeat: no-repeat;
+    background-color: rgb(8, 44, 68);
+    background-size: COVER;
+    background-position: center center;
+    visibility: visible;
+    animation-name: fadeInUp;">
+    <div style="background-color: #ffffff;
+    border: 1px solid black;
+    opacity: 0.6;padding: 150px">
+      <div  style="    font-weight: bold;
+      text-align: center;
+      color: #000000;
+      padding-top: 100px;
+	  padding-bottom: 50px;
+      font-size: 40px;
+  ">
+        CHÀO MỪNG BẠN ĐẾN TRANG QUẢN LÝ
+        </div>
+        <div style="text-align: center;
+        color: #000000  ;
+        padding-bottom: 50px;
+        font-size: 21px;
+        font-weight: bold;">
+          <p>Đến với nhau là một <span style="color: red">sự khởi đầu</span>. Giữ được nhau là <span style="color: red">sự tiến triển</span>. Làm việc cùng nhau là <span style="color: red">sự thành công</span>.</p>
+          </div>
+    </div>
+       </div>
+                            
+                               
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    <!-- ============================================================== -->
+                    <!-- end pageheader  -->
+                    <!-- ============================================================== -->
+                  {{--    <div class="ecommerce-widget">
+                        <div class="row">
+                            <div class="col-xl-12 col-lg-8 col-md-8 col-sm-12 col-12 ">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="text-muted">Số lượng nhân viên</h5>
+                                        <div class="metric-value d-inline-block">
+                                            <h1 class="mb-1">$12099</h1>
+                                        </div>
+                                        <div class="metric-label d-inline-block float-right text-success font-weight-bold">
+                                            <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
+                                        </div>
+                                    </div>
+                                    <div id="sparkline-revenue"></div>
+                                </div>
+                            </div>
+                           <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="text-muted">Affiliate Revenue</h5>
+                                        <div class="metric-value d-inline-block">
+                                            <h1 class="mb-1">$12099</h1>
+                                        </div>
+                                        <div class="metric-label d-inline-block float-right text-success font-weight-bold">
+                                            <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
+                                        </div>
+                                    </div>
+                                    <div id="sparkline-revenue2"></div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="text-muted">Refunds</h5>
+                                        <div class="metric-value d-inline-block">
+                                            <h1 class="mb-1">0.00</h1>
+                                        </div>
+                                        <div class="metric-label d-inline-block float-right text-primary font-weight-bold">
+                                            <span>N/A</span>
+                                        </div>
+                                    </div>
+                                    <div id="sparkline-revenue3"></div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="text-muted">Avg. Revenue Per User</h5>
+                                        <div class="metric-value d-inline-block">
+                                            <h1 class="mb-1">$28000</h1>
+                                        </div>
+                                        <div class="metric-label d-inline-block float-right text-secondary font-weight-bold">
+                                            <span>-2.00%</span>
+                                        </div>
+                                    </div>
+                                    <div id="sparkline-revenue4"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                           
+                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card">
+                                    <h5 class="card-header">Customer Acquisition</h5>
+                                    <div class="card-body">
+                                        <div class="ct-chart ct-golden-section" style="height: 354px;"></div>
+                                        <div class="text-center">
+                                            <span class="legend-item mr-2">
+                                                    <span class="fa-xs text-primary mr-1 legend-tile"><i class="fa fa-fw fa-square-full"></i></span>
+                                            <span class="legend-text">Returning</span>
+                                            </span>
+                                            <span class="legend-item mr-2">
+
+                                                    <span class="fa-xs text-secondary mr-1 legend-tile"><i class="fa fa-fw fa-square-full"></i></span>
+                                            <span class="legend-text">First Time</span>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </div> --}}
+            <!-- ============================================================== -->
+           
+            <!-- ============================================================== -->
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- end wrapper  -->
+
+@endsection
